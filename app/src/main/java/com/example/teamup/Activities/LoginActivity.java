8cfc00c8801973f3c937f0e5f7a9e2f63c73ee0b
nysimpson <natsimpson@gmail.com>
2020-04-07 19:28:22 -0400

    First App Commit

---
+++ app/src/main/java/com/example/teamup/Activities/LoginActivity.java
@@ -0,0 +1,135 @@
+package com.example.teamup.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.teamup.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+//----------------------------------------------------------------------------------
+// The activity that allows a user to Login
+// Also a feature to take a user to a registration activity
+//----------------------------------------------------------------------------------
+public class LoginActivity extends AppCompatActivity {
+
+    private final String LOGGED = "LOGGED";
+    private EditText etUsername;
+    private EditText etPassword;
+    private TextView btnSignUp;
+    private Button btnSignIn;
+    private ImageView imvIcon;
+    //----------------------------------------------------------------------------------
+    // Set the view
+    //----------------------------------------------------------------------------------
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ui_login);
+        ParseUser.getCurrentUser().logOut();
+
+        etUsername = findViewById(R.id.login_username);
+        etPassword = findViewById(R.id.login_password);
+        imvIcon = findViewById(R.id.login_icon);
+        btnSignIn = findViewById(R.id.signin_btn);
+        btnSignUp = findViewById(R.id.btn_SignUp);
+
+        //----------------------------------------------------------------------------------
+        // Listen for a click on the Sign In Button
+        //----------------------------------------------------------------------------------
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+
+                Log.d("LOGIN", "Info: " + username + " " + password);
+
+                if (username.length() == 0 || password.length() == 0) {
+                    Toast.makeText(LoginActivity.this, "Fields cannot be left blank", Toast.LENGTH_SHORT).show();
+                } else {
+                    login(username, password);
+                }
+            }
+        });
+
+        //----------------------------------------------------------------------------------
+        // Listen for a click on the Sign Up Button
+        //----------------------------------------------------------------------------------
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goUserRegistration();
+            }
+        });
+
+    }
+
+    //----------------------------------------------------------------------------------
+    // Functionality for the Sign Up Button
+    // Takes user to the User Registration Activity to register
+    //----------------------------------------------------------------------------------
+    private void goUserRegistration() {
+        Intent i = new Intent(this, UserRegistrationActivity.class);
+        startActivity(i);
+        Log.d("WORKING", "It's working");
+        finish();
+    }
+
+    //----------------------------------------------------------------------------------
+    // Functionality for the Sign In Button
+    // This function logs in the the user
+    // This function validates the user credentials
+    //----------------------------------------------------------------------------------
+    private void login(String user, String password) {
+
+        //Query to check user input against object (username)
+        ParseQuery<ParseObject> userQuery = ParseQuery.getQuery("User");
+        userQuery.whereEqualTo("username", user);
+        userQuery.whereEqualTo("password", password);
+        userQuery.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+
+                //Log.d("TAG", "Amount:" + objects.size());
+                if (objects != null) {
+                    Log.d("users", "Retrieved " + objects.get(0).get("username"));
+
+                    Log.d("users", "Retrieved " + objects.size() + " users");
+
+                    String Id = objects.get(0).getObjectId();
+
+                    goMainActivity(Id);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Invalid user credentials", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    //----------------------------------------------------------------------------------
+    // This function takes the user to the Main Activity after they were
+    // successfully logged in
+    //----------------------------------------------------------------------------------
+    private void goMainActivity(String id) {
+        Intent intent = new Intent(getBaseContext(), MainActivity.class);
+        intent.putExtra("username_ID", id);
+        startActivity(intent);
+        finish();
+    }
+}
+
---
+++ app/src/main/java/com/example/teamup/Activities/MainActivity.java
@@ -0,0 +1,185 @@
+package com.example.teamup.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.widget.Toolbar;
+
+
+import com.example.teamup.R;
+import com.google.android.material.navigation.NavigationView;
+//import com.example.teamup.fragments.ProfileFragment;
+
+//----------------------------------------------------------------------------------
+// The activity that sets up the framework for the app
+// Shows the main page the user gets redirected to
+//----------------------------------------------------------------------------------
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private String user_id;
+    private static final String LISTORDERS = "LISTORDERS";
+    private final String WHERE = "WHERE";
+    private final String CATEGORY = "CATEGORY";
+    private final String CART = "CART";
+    private final FragmentManager fragmentManager = getSupportFragmentManager();
+    private ImageView back;
+    private Boolean home = false;
+    private DrawerLayout drawer;
+
+
+
+    //----------------------------------------------------------------------------------
+    // Sets the view
+    //----------------------------------------------------------------------------------
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        ImageView back = findViewById(R.id.back_btn);
+        setSupportActionBar(toolbar);
+        //customer_id = getIntent().getStringExtra("customer_ID");
+
+
+        //----------------------------------------------------------------------------------
+        // Create Navigation Drawer
+        //----------------------------------------------------------------------------------
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //----------------------------------------------------------------------------------
+        //Set Container for Fragments
+        //----------------------------------------------------------------------------------
+        //CategoryFragment homeFragment = new CategoryFragment(cart);
+        //fragmentManager.beginTransaction()
+          //      .replace(R.id.flContainer, homeFragment, CATEGORY)
+            //    .addToBackStack(CATEGORY)
+              //  .commit();
+
+        //----------------------------------------------------------------------------------
+        // Functionality for back button
+        //----------------------------------------------------------------------------------
+        back.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.d("CLICK", "click works");
+                fragmentManager.popBackStack();
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+        switch(item.getItemId()){
+
+            case R.id.nav_home:
+                //CategoryFragment homeFragment = new CategoryFragment(cart);
+                fragmentManager.beginTransaction()
+                        //.replace(R.id.flContainer, homeFragment, CATEGORY)
+                        .addToBackStack(CATEGORY)
+                        .commit();
+                break;
+            case R.id.nav_friends:
+                break;
+            case R.id.nav_groups:
+
+                //ListOrdersFragment ordersFragment = new ListOrdersFragment(customer_id, cart);
+                fragmentManager.beginTransaction()
+                       // .replace(R.id.flContainer, ordersFragment, LISTORDERS)
+                        .addToBackStack(CATEGORY)
+                        .commit();
+                break;
+            case R.id.nav_settings:
+                break;
+
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    //----------------------------------------------------------------------------------
+    //Functionality to move navigation back off screen
+    //----------------------------------------------------------------------------------
+    @Override
+    public void onBackPressed(){
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+
+    //----------------------------------------------------------------------------------
+    //Show toolbar
+    //----------------------------------------------------------------------------------
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_top_nav, menu);
+
+        return true;
+    }
+
+    //----------------------------------------------------------------------------------
+    //Creating Functionality for buttons on toolbar
+    //----------------------------------------------------------------------------------
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        //Fragment fragment = new CategoryFragment(cart);
+
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                //TODO: swap fragment here
+                break;
+            case R.id.action_shopcart:
+                //fragment = new CartFragment(cart);
+                break;
+            default:
+        }
+
+        fragmentManager.beginTransaction()
+               // .replace(R.id.flContainer, fragment)
+                .addToBackStack(CART)
+                .commit();
+
+
+        return true;
+        //return super.onOptionsItemSelected(item);
+    }
+
+
+    public void hideButton(Boolean b){
+        home = b;
+    }
+
+
+}
